6c76879f888d223e0d3f177a6fc91e2d6641bd29
Mark Broski <mark@mbroski.com>
2015-07-06 10:43:17 -0400

    This is my initial commit of the project framework

---
+++ MSBTextFieldDelegates/MSBTextFieldDelegates/MSBTextFieldDelegates.h
@@ -0,0 +1,19 @@
+//
+//  MSBTextFieldDelegates.h
+//  MSBTextFieldDelegates
+//
+//  Created by Mark Broski on 7/6/15.
+//  Copyright (c) 2015 Mark Broski. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MSBTextFieldDelegates.
+FOUNDATION_EXPORT double MSBTextFieldDelegatesVersionNumber;
+
+//! Project version string for MSBTextFieldDelegates.
+FOUNDATION_EXPORT const unsigned char MSBTextFieldDelegatesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MSBTextFieldDelegates/PublicHeader.h>
+
+
